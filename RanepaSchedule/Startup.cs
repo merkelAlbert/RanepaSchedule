72e6c52c59ffc420dc72de9bda7960818a512ec3
merkelAlbert <merkel.albert1997@gmail.com>
2018-11-05 04:16:58 +0300

    downgrade for hosting

--- RanepaSchedule/Startup.cs
+++ RanepaSchedule/Startup.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using RanepaSchedule.Services;
 
 namespace RanepaSchedule
@@ -25,7 +17,7 @@ namespace RanepaSchedule
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
             services.AddScoped<ScheduleService>();
         }
 
@@ -34,10 +26,6 @@ namespace RanepaSchedule
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
             }
 
             app.UseMvc();
